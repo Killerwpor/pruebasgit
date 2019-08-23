fd0e07c214fa43b17c7043f7855dd9c85e7f5ebc
pedrocalle100 <pedrocalle100@gmail.com>
2019-08-23 15:50:59 -0500

    comienzo de dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginCardComponent } from './login-card/login-card.component';
 import { AlertModalComponent } from './alert-modal/alert-modal.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginCardComponent,
-    AlertModalComponent
+    AlertModalComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
