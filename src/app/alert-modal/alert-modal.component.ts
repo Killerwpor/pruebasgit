a69b27d6410f277681a70aec95bd990213ce8f34
Pedro Calle Jaramillo <36971821+pcallej@users.noreply.github.com>
2019-08-23 14:52:06 -0500

    Initial commit

---
+++ src/app/alert-modal/alert-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'alert-modal',
+  templateUrl: './alert-modal.component.html',
+  styleUrls: ['./alert-modal.component.css']
+})
+export class AlertModalComponent {
+
+  alert_modal_headline = "Lorem Ipsum Noctis Lucis Caellum";
+
+}
