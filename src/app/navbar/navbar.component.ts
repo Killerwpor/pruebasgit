26303eb14804797d8823ae2a0d9a72ed2a7d9532
pedrocalle100 <pedrocalle100@gmail.com>
2019-09-19 16:19:41 -0500

    comunicacion entre componente hijo y componente padre, toggler de barra de contactos en modo mobil

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,28 +1,37 @@
-import { element } from 'protractor';
-import { Component, OnInit } from '@angular/core';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
-import * as $ from 'jquery';
+import { element } from "protractor";
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
+import * as $ from "jquery";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"]
 })
 export class NavbarComponent implements OnInit {
+  constructor(private modalService: NgbModal) {}
 
-  
-  constructor(private modalService: NgbModal) { }
-
+  // sidebar
   private _opened: boolean = false;
-
   private _toggleSidebar() {
     this._opened = !this._opened;
   }
 
-  closeResult: string;
 
-  ngOnInit() {
-    
+
+  // contactpanel
+  @Output() open: EventEmitter<any> = new EventEmitter();
+  @Output() close: EventEmitter<any> = new EventEmitter();
+  visible: boolean = true;
+
+  toggle() {
+    this.visible = !this.visible;
+    if (this.visible) {
+      this.open.emit(null); //emit event here
+    } else {
+      this.close.emit(null);
+    }
   }
 
+  ngOnInit() {}
 }
