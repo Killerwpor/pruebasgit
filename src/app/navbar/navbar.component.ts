b9d1226f18b6789362491086ec53ed8a294615ef
pedrocalle100 <pedrocalle100@gmail.com>
2019-09-19 12:16:04 -0500

    arreglo en layout, arreglo de propiedades de bootstrap

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { element } from 'protractor';
 import { Component, OnInit } from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 import * as $ from 'jquery';
@@ -11,24 +12,6 @@ export class NavbarComponent implements OnInit {
 
   
   constructor(private modalService: NgbModal) { }
-  
-  open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return  `with: ${reason}`;
-    }
-  }
 
   private _opened: boolean = false;
 
