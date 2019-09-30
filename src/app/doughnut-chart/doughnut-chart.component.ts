84bae9f5df4a873d5c9bf19740db8839e7259e2a
pedrocalle100 <pedrocalle100@gmail.com>
2019-09-30 16:40:42 -0500

    cambio de la lista para escoger el progreso

--- src/app/doughnut-chart/doughnut-chart.component.ts
+++ src/app/doughnut-chart/doughnut-chart.component.ts
@@ -1,53 +1,25 @@
-import { ChartDataSets } from 'chart.js';
-import { Component, OnInit, Input } from '@angular/core';
-import { MultiDataSet, Label } from 'ng2-charts';
+import { ChartDataSets } from "chart.js";
+import { Component, OnInit, Input } from "@angular/core";
+import { MultiDataSet, Label } from "ng2-charts";
 
 @Component({
-  selector: 'app-doughnut-chart',
-  templateUrl: './doughnut-chart.component.html',
-  styleUrls: ['./doughnut-chart.component.css']
+  selector: "app-doughnut-chart",
+  templateUrl: "./doughnut-chart.component.html",
+  styleUrls: ["./doughnut-chart.component.css"]
 })
 export class DoughnutChartComponent implements OnInit {
-
-  public doughnutChartOptions={
+  public doughnutChartOptions = {
     responsive: true,
-    maintainAspectRation: true
-  }
+    maintainAspectRation: false,
+  };
 
   @Input() doughnutChartData: MultiDataSet;
   @Input() doughnutColors: Array<any>;
   @Input() doughnutChartLabels;
 
-  /*
-  
-  public doughnutChartData: MultiDataSet = [
-    [60,40]
-  ];
-
-  public doughnutColors: Array<any> = [
-    { 
-      backgroundColor: ['#40b987', 'rgba(0,0,0,0)'],
-      borderColor: '#40b987'
-    },
-    { 
-      backgroundColor: ['rgba(0, 229, 255,1.0)', 'rgba(0,0,0,0)'],
-      borderColor: 'rgba(0,0,0,0)'
-    },
-    { 
-      backgroundColor: ['rgba(255, 82, 82,1.0)', 'rgba(0,0,0,0)'],
-      borderColor: 'rgba(0,0,0,0)'
-    }
-  ]
-
-  public doughnutChartLabels = ["Completo", "Faltante"];
-  
-  
-  */
   public doughnutChartType = "doughnut";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
